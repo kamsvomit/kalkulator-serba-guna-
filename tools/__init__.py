bb9ca0c857f388c46edc7919a8487243fd819692
kamsvomit <36455454+kamsvomit@users.noreply.github.com>
2026-02-18 20:09:28 +0700

    Create __init__.py

---
+++ tools/__init__.py
@@ -0,0 +1,10 @@
+from tools.math import registry as math_registry
+# from tools.converter import registry as converter_registry
+# from tools.text      import registry as text_registry
+
+# Tambah baris import di sini kalau ada file kategori baru
+tools_registry = {
+    **math_registry,
+    # **converter_registry,
+    # **text_registry,
+}
